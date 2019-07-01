d93e401b92192eb968d2455265e3ef91cebbc767
zenghaiqing <807111582@qq.com>
2019-07-01 10:13:18 +0800

    login更新

--- src/views/login.vue
+++ src/views/login.vue
@@ -11,14 +11,12 @@
       />
       <van-field v-model="password" type="password" placeholder="请输入密码" />
     <!-- </van-cell-group> -->
-    <div>
+    <div class="connect">
       <a href="#">短信验证码登录</a>
       <a href="#">免费注册 </a>
-      <van-button plain type="primary">短信验证码登录</van-button>
-      <van-button plain type="danger">免费注册</van-button>
     </div >
-    <div>
-      <van-button round type="danger" size="large" >登录</van-button>
+    <div >
+      <van-button round  size="large" >登录</van-button>
     </div>
   </div>
 </template>
@@ -36,16 +34,51 @@ export default {
 <style lang='scss'>
   @import '../assets/px2rem.scss';
   .van-cell{
-      border-bottom: 1px  solid red;
-      margin: px2rem(10) auto
+      
+      margin: 0 auto
   }
   .logo{
         display: block;
-        width: px2rem(80) !important;
+        width: px2rem(80);
         height: px2rem(80);
-        margin: 0 auto
+        margin: px2rem(35) auto 0;
+        
           }
   .van-button{
-    margin: 0 10px auto
+    margin: 45px auto;
+    display: flex;
+    background:-webkit-linear-gradient(left,#ff9000,#ff5000) no-repeat ;
+    width: px2rem(329);
+    height: px2rem(45);
+    font-size: px2rem(16);
+    justify-content: center;
+      .van-button__text{
+        display: flex;
+        
+        
+      }
   }
+  .connect{
+    margin: px2rem(22) auto 0;
+    display: flex;
+    width:px2rem(329);
+    justify-content: space-between;
+    font-size: px2rem(14);
+    
+      a{
+        color: #000;
+      }
+    
+    // padding: 0  px2rem(23)
+  }
+  .van-field__control{
+    display: flex;
+    margin: 0 auto;
+    width: px2rem(329);
+    // height: px2rem(20);
+    font-size: px2rem(16);
+    padding-top:px2rem(50);
+    border-bottom: 1px  solid red;
+  }
+
 </style>
